df19b77810668f0791f97ea95f323a4c6c32828e
Robson Roberto Souza Peixoto <robsonpeixoto@gmail.com>
2021-09-27 08:33:15 -0300

    chore: enable ecto stats on phoenix live dashboard

--- mix.exs
+++ mix.exs
@@ -42,6 +42,7 @@ defmodule Hello.MixProject do
       {:phoenix_live_view, "~> 0.16.0"},
       {:floki, ">= 0.30.0", only: :test},
       {:phoenix_live_dashboard, "~> 0.5"},
+      {:ecto_psql_extras, "~> 0.7"},
       {:esbuild, "~> 0.2", runtime: Mix.env() == :dev},
       {:swoosh, "~> 1.3"},
       {:telemetry_metrics, "~> 0.6"},
