04a7dd1e61d8010aedee58d8ae102281c9d5fcd9
Robson Roberto Souza Peixoto <robsonpeixoto@gmail.com>
2021-09-27 09:56:10 -0300

    chore: install dialyxir

--- mix.exs
+++ mix.exs
@@ -50,7 +50,8 @@ defmodule Hello.MixProject do
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:credo, "~> 1.5", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.0", only: [:dev], runtime: false}
     ]
   end
 
