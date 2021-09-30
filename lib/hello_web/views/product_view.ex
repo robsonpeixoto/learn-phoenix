ed4109401c1e7039cd41b9eecac4438efceeedaf
Robson Roberto Souza Peixoto <robsonpeixoto@gmail.com>
2021-09-30 09:28:15 -0300

    feature: define product category

--- lib/hello_web/views/product_view.ex
+++ lib/hello_web/views/product_view.ex
@@ -1,3 +1,16 @@
 defmodule HelloWeb.ProductView do
   use HelloWeb, :view
+
+  def category_select(f, changeset) do
+    existing_ids =
+      changeset
+      |> Ecto.Changeset.get_field(:categories)
+      |> Enum.map(& &1.id)
+
+    category_opts =
+      for cat <- Hello.Catalog.list_categories(),
+          do: [key: cat.title, value: cat.id, selected: cat.id in existing_ids]
+
+    multiple_select(f, :category_ids, category_opts)
+  end
 end
